54557abaa290fdde85861555dbc03b2cf2a35b87
MoisesGJ <mgarciaj1802@alumno.ipn.mx>
2023-11-27 22:01:00 -0600

    Add rules responsive schools

--- Components/Schools/index.jsx
+++ Components/Schools/index.jsx
@@ -1,83 +1,96 @@
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import styles from './Image.module.css';
 
 import Image from 'next/image';
 
-import Slider from 'react-slick';
-
 export default function Schools() {
-  const settings = {
-    dots: false,
-    infinite: false,
-    centerMode: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-  };
   return (
     <section>
       <h2 className="text-center p-5 mt-5 text-xl font-semibold text-accent">
         Las mejores viviendas cerca de las mejores escuelas
       </h2>
 
-      <div className="flex justify-center items-center">
-        <Slider {...settings}>
-          <div>
-            <Image
-              src={'/escuelas/ipn.webp'}
-              alt="Hola"
-              width={64}
-              height={64}
-              className={`object-cover ` + styles.school}
-            />
-          </div>
-          <div>
-            <Image
-              src={'/escuelas/ipn.webp'}
-              alt="Hola"
-              width={64}
-              height={64}
-              className={`object-cover ` + styles.school}
-            />
-          </div>
-          <div>
-            <Image
-              src={'/escuelas/ipn.webp'}
-              alt="Hola"
-              width={64}
-              height={64}
-              className={`object-cover ` + styles.school}
-            />
-          </div>
-          <div>
-            <Image
-              src={'/escuelas/ipn.webp'}
-              alt="Hola"
-              width={64}
-              height={64}
-              className={`object-cover ` + styles.school}
-            />
-          </div>
-          <div>
-            <Image
-              src={'/escuelas/ipn.webp'}
-              alt="Hola"
-              width={64}
-              height={64}
-              className={`object-cover ` + styles.school}
-            />
-          </div>
-          <div>
-            <Image
-              src={'/escuelas/ipn.webp'}
-              alt="Hola"
-              width={64}
-              height={64}
-              className={`object-cover ` + styles.school}
-            />
-          </div>
-        </Slider>
+      <div className="flex flex-wrap gap-3 justify-center items-center">
+        <div>
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div>
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div className="hidden md:block">
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div className="hidden md:block">
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div>
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div className="hidden md:block">
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div className="hidden lg:block">
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div className="hidden lg:block">
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
+        <div className="hidden lg:block">
+          <Image
+            src={'/escuelas/ipn.webp'}
+            alt="Hola"
+            width={64}
+            height={64}
+            className={`object-cover ` + styles.school}
+          />
+        </div>
       </div>
     </section>
   );
