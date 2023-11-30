f7df6bfd04b3b148978b0f42e7aa2d2b933e4f01
Hector <hectors.gpt@gmail.com>
2023-11-29 19:25:39 -0600

    Component aded

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 import TestimonialesCard from '@/Components/TestimonialesCard';
 import ButtonLP from '@/Components/ButtonLP';
+import SearchBar from '@/Components/SearchBar';
 
 export default function Home() {
   return (
@@ -17,24 +18,9 @@ export default function Home() {
       {/*///////////////////////// Info///////////////////////*/}
 
       {/*///////////////////////// Buscador///////////////////////*/}
-      <div className="flex justify-center items-center my-5">
-        <div className="rounded-xl border-2 border-primary focus:outline-none focus:border-accent flex w-[80%] ">
-          <Image 
-            src={'/Icons/lupa.svg'}
-            width={22}
-            height={22}
-            alt="Cuarto"
-            className="mx-3"
-          />
-          <input
-            type="text"
-            placeholder="Mi proximo departamento será..."
-            className="max-w-[260px] w-full py-3 focus:outline-none"
-          />
-        </div>  
-      </div>
-    
-
+<SearchBar>
+  
+</SearchBar>
       {/* HECTOR */}
       
       {/* MOISÉS */}
